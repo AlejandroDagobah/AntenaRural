1d3e82dab735ce13046bcdb5cb81ea3f95b072ea
AlejandroDagobah <alejandrodagobah@gmail.com>
2022-05-15 13:21:31 -0500

    Mapa y Animaciónes de Personajes
    mapa y animaciónes

---
+++ Antenas Game/Assets/Scripts/npcAI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcAI : MonoBehaviour
+{
+    [SerializeField]
+    float m_npcSpeed;
+
+    [SerializeField]
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
